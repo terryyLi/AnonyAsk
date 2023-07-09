fdace071a35c08afe5daa5e9afc6b9f94baee79b
terryyLi <jielinl@andrew.cmu.edu>
2023-07-09 14:31:23 -0500

    set up sentiment analysis tool

--- backend/routes/Conversation.js
+++ backend/routes/Conversation.js
@@ -80,10 +80,37 @@ router.post('/:postId', async (req, res) => {
     }
 });
 
+// Imports the Google Cloud client library
+const language = require('@google-cloud/language');
+
+// Creates a client
+const client = new language.LanguageServiceClient();
+
+
 // Route to add an answer to a conversation
 router.post('/:conversationId/answer', async (req, res) => {
     const { conversationId } = req.params;
     const { content } = req.body;
+
+    const document = {
+        content: content,
+        type: 'PLAIN_TEXT',
+    };
+
+    // Detects the sentiment of the document
+    const [result] = await client.analyzeSentiment({document});
+
+    const sentiment = result.documentSentiment;
+    console.log('Document sentiment:');
+    console.log(`  Score: ${sentiment.score}`);
+    console.log(`  Magnitude: ${sentiment.magnitude}`);
+
+    const sentences = result.sentences;
+    sentences.forEach(sentence => {
+    console.log(`Sentence: ${sentence.text.content}`);
+    console.log(`  Score: ${sentence.sentiment.score}`);
+    console.log(`  Magnitude: ${sentence.sentiment.magnitude}`);
+    });
 
     try {
     // Find the conversation by ID
